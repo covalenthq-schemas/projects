2f7b8bc042648ec480faa4e1b6468d3cb1f42c67
Ganesh Swami <iamganesh.swami@gmail.com>
2019-09-26 15:41:39 -0700

    calculate cdai quotes

--- schema/instadapp/CompoundDai.js
+++ schema/instadapp/CompoundDai.js
@@ -1,12 +1,25 @@
 import { CDAI_CONTRACT } from "./constants";
 
 cube(`CompoundDai`, {
-    sql: `select 
+    sql: `
+
+with prices as (
+    select symbol, price from crawl.prices
+    where base = 'USD' and symbol in ('CDAI', 'CBAT', 'CETH', 'CREP', 'CUSDC', 'CWBTC', 'CZRX', 'ETH')
+)
+select
    data as cdai_balance,      
-        key_path->1 as key_path,
-        row_number() over (partition by key_path->1 order by block_id desc, emit_seq desc) as rn
-        from batch.trace_sstore_events tse
-    where tse.account = '${CDAI_CONTRACT}'
+   key_path->1 as key_path,
+   row_number() over (partition by key_path->1 order by block_id desc, emit_seq desc) as rn,
+   p.price as price
+   from batch.trace_sstore_events tse
+
+left join (
+    select * from prices
+) p
+on p.symbol = 'CDAI' 
+
+where tse.account = '${CDAI_CONTRACT}'
     and key_path->0 = '15'::jsonb
 ` ,
 
@@ -14,8 +27,21 @@ cube(`CompoundDai`, {
         cDai_balance: {
             type: `sum`,
             sql: `coalesce(cdai_balance, 0) / 1e8`,
-            format: `currency`
+            format: `currency`,
+            title: "cDAI balance"
         },
+        cDai_USD_quote_rate: {
+            type: `sum`,
+            sql: `price`,
+            format: `currency`,
+            title: "cDAI USD quote"
+        },
+        cDai_USD_balance_quote: {
+            type: `sum`,
+            sql: `price * coalesce(cdai_balance, 0) / 1e8`,
+            format: `currency`,
+            title: "cDAI balance quote"
+        }        
     },
     dimensions: {
         keyPath: {
