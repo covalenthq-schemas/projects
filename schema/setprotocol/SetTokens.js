17dc49dd77a8dce675a6f837121c78fcad55caf6
Ganesh Swami <iamganesh.swami@gmail.com>
2019-09-28 00:04:15 -0700

    rewrite query to be more performant

--- schema/setprotocol/SetTokens.js
+++ schema/setprotocol/SetTokens.js
@@ -5,77 +5,75 @@ import { SET_TOKEN_CREATED_EVENT, SET_CORE } from "./constants";
 // components 0x2260fac5e5542a773aa44fbcfedf7c193bc2c599, 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2 
 
 // 2 - supply
-// 20, 14 underlying set 
+// 20, 14 - underlying set 
+// 12 - natural unit
+// 15 - units for rebalancing
+// 8 - units for static
+const BLOCK_ID_START = 1390900854189650515;
 
 cube('SetTokens', {
     sql: `
-    with staticsets as (
-        select 'Static Set' as set_type, substr(topics[2], 13) as set_token_address,
-        hex_to_int(encode(substr(topics[2], 13), 'hex')) as decimal_addr
-        from live.block_log_events
-        where not (
-            substr(encode(data, 'hex'), 25, 40) = 'd85af84c22b71bdaa25333a7898ddc6f2f1088eb' or 
-            substr(encode(data, 'hex'), 25, 40) = 'ad78e5570f24a268687c6cc0f73966e9978568a7' or
-            substr(encode(data, 'hex'), 25, 40) = '15518cdd49d83471e9f85cdcfbd72c8e2a78dde2'            
-        ) and
-        topics[1] = '${SET_TOKEN_CREATED_EVENT}'
-        AND sender = '${SET_CORE}'    
-    ),
-    rebalancingsets as (
-        select 'Rebalancing Set' as set_type, substr(topics[2], 13) as set_token_address
-        from live.block_log_events
-        where (
-            substr(encode(data, 'hex'), 25, 40) = 'd85af84c22b71bdaa25333a7898ddc6f2f1088eb' or 
-            substr(encode(data, 'hex'), 25, 40) = 'ad78e5570f24a268687c6cc0f73966e9978568a7' or
-            substr(encode(data, 'hex'), 25, 40) = '15518cdd49d83471e9f85cdcfbd72c8e2a78dde2'            
-        ) and
-        topics[1] = '${SET_TOKEN_CREATED_EVENT}'
-        AND sender = '${SET_CORE}'        
-    )
-    
+
     select set_type, 
     '0x' || encode(set_token_address, 'hex') as set_token_address,
+    '0x' || encode(base_token_address, 'hex') as base_token_address,
     supply_tbl.supply,
-    '0x' || encode(baseset_tbl.base_token_address, 'hex') as base_token_address
-    from (
-        select set_type, 
-        set_token_address
-        from staticsets
-        union 
-        select set_type, 
-        set_token_address
-        from rebalancingsets
-    ) lhs
-    
+    units_tbl.units,
+    nunits_tbl.naturalunit
+
+    from stb_pre_aggregations.sp_token_base lhs
+
     left join
     (
         select account, coalesce(data, 0) / 1e18 as supply,
         row_number() over (partition by account order by block_id desc, emit_seq desc) as rn
-        from batch.trace_sstore_events_201909
-        where account in (
-            select set_token_address from staticsets
-            union select set_token_address from rebalancingsets
-        ) and
-        key_path->0 = '2'
+        from (
+            select * from batch.trace_sstore_events
+        	where block_id > ${BLOCK_ID_START} 
+        	and account in (select set_token_address from stb_pre_aggregations.sp_token_base)
+        	and key_path->0 = '2'
+        ) x
     ) supply_tbl
     on supply_tbl.account = lhs.set_token_address and supply_tbl.rn = 1
-    
+
     left join
     (
-        select ss.set_token_address as base_token_address, 
-        account,
+        select account, data as units,
         row_number() over (partition by account order by block_id desc, emit_seq desc) as rn
-        from batch.trace_sstore_events_201909 tse 
-        join ( 
-            select * from staticsets
-        ) ss
-        on tse.data = ss.decimal_addr
-        where key_path->0 = '14'::jsonb
-        and account in (
-            select set_token_address from rebalancingsets
-        )
-    ) baseset_tbl
-    on baseset_tbl.account = lhs.set_token_address and baseset_tbl.rn = 1
+        from (
+            select * from batch.trace_sstore_events
+        	where block_id > ${BLOCK_ID_START} 
+        	and account in (select set_token_address from stb_pre_aggregations.sp_token_base where set_type = 'Rebalancing Set')
+        	and key_path->0 = '15'
+        ) x
+
+        union 
+        
+        select account, data as units,
+        row_number() over (partition by account order by block_id desc, emit_seq desc) as rn
+        from (
+            select * from batch.trace_sstore_events
+        	where block_id > 1424239138687365400 
+        	and account in (select set_token_address from stb_pre_aggregations.sp_token_base where set_type = 'Static Set')
+        	and key_path->0 = '8'
+        ) x
+
+    ) units_tbl
+    on units_tbl.account = lhs.set_token_address and units_tbl.rn = 1
+
+    left join
+    (
+        select account, data as naturalunit,
+        row_number() over (partition by account order by block_id desc, emit_seq desc) as rn
+        from (
+            select * from batch.trace_sstore_events
+        	where block_id > ${BLOCK_ID_START} 
+        	and account in (select set_token_address from stb_pre_aggregations.sp_token_base where set_type = 'Rebalancing Set')
+        	and key_path->0 = '12'
+        ) x
+    ) nunits_tbl
+    on nunits_tbl.account = lhs.set_token_address and nunits_tbl.rn = 1    
+
     `,
     measures: {
         count: {
@@ -92,6 +90,14 @@ cube('SetTokens', {
         price_usd: {
             type: `avg`,
             sql: `0`
+        },
+        units: {
+            sql: `units`,
+            type: `sum`
+        },
+        naturalUnit: {
+            sql: `naturalunit`,
+            type: `sum`
         }
     },
     dimensions: {
@@ -120,5 +126,10 @@ cube('SetTokens', {
             sql: `base_token_address`,
             type: `string`
         }
-    }
+    },
+    preAggregations: {
+        main: {
+            type: `originalSql`
+        }
+    }    
 });
