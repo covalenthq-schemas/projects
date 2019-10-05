5414ef41eb5b42bb9844ab5746ac60b9e25a4ca4
Ganesh Swami <iamganesh.swami@gmail.com>
2019-10-05 13:48:37 -0700

    fix name

--- schema/compound/CompoundBorrows.js
+++ schema/compound/CompoundBorrows.js
@@ -103,21 +103,17 @@ cube(`Borrows`, {
             sql: `logged_borrower`,
             type: `string`
         },
-        SenderAddress: {
-            sql: `sender`,
-            type: `string`
-        },
-        SenderName: {
+        BorrowToken: {
             type: `string`,
             case: {
                 when: [
-                    { sql: `sender = '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5'`, label: 'cETH' },
-                    { sql: `sender = '0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e'`, label: 'cBAT' },
-                    { sql: `sender = '0xf5dce57282a584d2746faf1593d3121fcac444dc'`, label: 'cDAI' },
-                    { sql: `sender = '0x158079ee67fce2f58472a96584a73c7ab9ac95c1'`, label: 'cREP' },
-                    { sql: `sender = '0x39aa39c021dfbae8fac545936693ac917d5e7563'`, label: 'cUSDC' },
-                    { sql: `sender = '0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407'`, label: 'cZRX' },
-                    { sql: `sender = '0xc11b1268c1a384e55c48c2391d8d480264a3a7f4'`, label: 'cWBTC' },
+                    { sql: `sender = '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5'`, label: 'ETH' },
+                    { sql: `sender = '0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e'`, label: 'BAT' },
+                    { sql: `sender = '0xf5dce57282a584d2746faf1593d3121fcac444dc'`, label: 'DAI' },
+                    { sql: `sender = '0x158079ee67fce2f58472a96584a73c7ab9ac95c1'`, label: 'REP' },
+                    { sql: `sender = '0x39aa39c021dfbae8fac545936693ac917d5e7563'`, label: 'USDC' },
+                    { sql: `sender = '0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407'`, label: 'ZRX' },
+                    { sql: `sender = '0xc11b1268c1a384e55c48c2391d8d480264a3a7f4'`, label: 'WBTC' },
                 ]
             }
         },
